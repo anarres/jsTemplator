33064968640ff377b64ce1c4c383ff5245ee0b06
Anarres <harrison9340@gmail.com>
2012-05-18 18:01:43 +0100

    x

--- betterSplit.js
+++ betterSplit.js
@@ -1,10 +1,14 @@
-function splitStringKeepDelimiters(myString, delimiter) {
+function splitStringKeepDelimiters(myString, delimiter, option) {
+
+    var delimiters = new Array();
+    var subStrings = new Array();
     var output = new Array();
 
     // Check if it starts with a delimiter
     if( myString.search(delimiter) == 0 ) {
         var dArray = myString.match(delimiter);
         var delimiter1 = dArray[0];
+        delimiters.push(delimiter1);
         output.push(delimiter1);
         if (myString.length > delimiter1.length) {
             myString = myString.slice(delimiter1.length);
@@ -15,6 +19,7 @@ function splitStringKeepDelimiters(myString, delimiter) {
     // Chew away at the string till it's gone
     while (1==1) {
         var word1 = myString.split(delimiter,1)[0];
+        subStrings.push(word1);
         output.push(word1);
         if (myString.length > word1.length) {
             myString = myString.slice(word1.length);
@@ -23,13 +28,22 @@ function splitStringKeepDelimiters(myString, delimiter) {
 
         var dArray = myString.match(delimiter);
         var delimiter1 = dArray[0];
+        delimiters.push(delimiter1);
         output.push(delimiter1);
         if (myString.length > delimiter1.length) {
             myString = myString.slice(delimiter1.length);
         }
         else { break; }
     }
-    return output;
+    if( option == "delimitersOnly" ) {
+        return delimiters;
+    }
+    else if ( option == "subStringsOnly" ) {
+        return subStrings;
+    }
+    else {
+        return output;
+    }
 }
 /*
 USAGE EXAMPLE
